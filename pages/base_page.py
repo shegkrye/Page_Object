2d532854bf7ae1403529dfa9c308d68b7a2560c1
ЗУБ <shegkryeui387t5iseu@mail.ru>
2020-09-07 12:39:07 +0300

    Работает

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By 
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    def open(self):
+        self.browser.get(self.url)
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+    
