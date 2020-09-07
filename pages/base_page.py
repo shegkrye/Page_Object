4bdeab296d5ff12786b1368cf691530cee1a3a46
ЗУБ <shegkryeui387t5iseu@mail.ru>
2020-09-07 12:48:32 +0300

    Проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By 
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -7,5 +8,10 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-    
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
